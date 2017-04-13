e5b0579cb47a04239599eb09f65344ba738df505
Sagar Pawar <pawarsgr18@gmail.com>
2017-04-13 20:45:10 +0530

    first time

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+;(function(){
+
+			// Menu settings
+			$('#menuToggle, .menu-close').on('click', function(){
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+
+
+})(jQuery)
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "70%";
+    // document.getElementById("flipkart-navbar").style.width = "50%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "rgba(0,0,0,0)";
+}
